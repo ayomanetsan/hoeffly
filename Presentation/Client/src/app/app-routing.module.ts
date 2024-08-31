5b671111475117c2483f27258a5bd0727ca49ac2
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2024-08-31 20:12:45 +0300

    ui: create landing page

--- Presentation/Client/src/app/app-routing.module.ts
+++ Presentation/Client/src/app/app-routing.module.ts
@@ -3,6 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('./landing/feature/landing/landing.module').then(
+        (m) => m.LandingModule
+      ),
+  },
+  {
     path: 'auth',
     loadChildren: () =>
       import('./auth/feature/auth-shell/auth-shell.module').then(
