277035495779084360df86e34fe3805879494a86
NureValterAnna <anna.valter@iqbuddy.com>
2025-02-17 14:39:13 +0200

    fix: add giftId to GiftResponse

--- Application/Gifts/Queries/GiftResponse.cs
+++ Application/Gifts/Queries/GiftResponse.cs
@@ -4,6 +4,8 @@ namespace Application.Gifts.Queries;
 
 public record GiftResponse
 {
+    public required Guid Id { get; set; }
+
     public required string Name { get; set; }
 
     public required Guid CategoryId { get; set; }
