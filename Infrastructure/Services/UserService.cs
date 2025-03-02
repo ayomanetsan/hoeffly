ce8a7ed82cc28ce116cae8665cd230c2e21177f1
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2025-03-02 20:25:13 +0200

    fix: update public users querying logic

--- Infrastructure/Services/UserService.cs
+++ Infrastructure/Services/UserService.cs
@@ -4,7 +4,11 @@ using Microsoft.AspNetCore.Http;
 
 namespace Infrastructure.Services;
 
-public class UserService(IRepository<User> userRepository, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) 
+public class UserService(
+    IRepository<User> userRepository, 
+    IRepository<Friendship> friendshipRepository, 
+    IUnitOfWork unitOfWork, 
+    IHttpContextAccessor httpContextAccessor) 
     : IUserService
 {
     public async Task CreateUserAsync(User user, CancellationToken cancellationToken)
@@ -23,10 +27,24 @@ public class UserService(IRepository<User> userRepository, IUnitOfWork unitOfWor
     public async Task<(IEnumerable<User> users, int totalPages)> GetUsersAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
     {
         var userEmail = GetUserEmailFromContext();
-        
+    
+        var currentUser = await userRepository.GetQueryable()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Email == userEmail, cancellationToken)
+            ?? throw new NotFoundException("Current user not found.");
+    
+        // Get the list of user IDs that are friends with the current user
+        var friendIds = await friendshipRepository.GetQueryable()
+            .AsNoTracking()
+            .Where(f => f.RequesterId == currentUser.Id || f.RecipientId == currentUser.Id)
+            .Select(f => f.RequesterId == currentUser.Id ? f.RecipientId : f.RequesterId)
+            .ToListAsync(cancellationToken);
+
+        // Query the users excluding those who are friends with the current user
         var publicUsersQuery = userRepository
             .GetQueryable()
-            .Where(u => u.IsPublic && u.Email != userEmail);
+            .AsNoTracking()
+            .Where(u => u.IsPublic && u.Email != userEmail && !friendIds.Contains(u.Id));
 
         var totalItems = await publicUsersQuery.CountAsync(cancellationToken);
         var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
@@ -37,7 +55,7 @@ public class UserService(IRepository<User> userRepository, IUnitOfWork unitOfWor
             .Skip((pageNumber - 1) * pageSize)
             .Take(pageSize)
             .ToListAsync(cancellationToken);
-    
+
         return (users, totalPages);
     }
     
